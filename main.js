8dc5488f77e8dd130337f39262782467208c745f
AdfalomnLaeninfus <adrianoreispereira88@gmail.com>
2021-12-08 12:11:35 -0300

    Update CSS

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const menuButtonHide = document.getElementById();
+const corPrimaria = "#FF5959"
+const corSegundaria = "#D382A9"
+const corTerciaria = "#83819F"
+const corQuaternaria = "#25242D"
+
+const menuButtonHide = document.getElementById("MenuCel");
+const celSubMenus = document.querySelector("nav");
+
+menuButtonHide.addEventListener("click", ()=>{
+    celSubMenus.classList.toggle("downMenu");
+});
